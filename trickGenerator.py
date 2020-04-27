7e101385a03d4aaf4fc38c3ff79f5f4011e06c84
Aya <47668950+AyaPK@users.noreply.github.com>
2020-04-27 18:18:43 +0100

    Update trickGenerator.py

--- trickGenerator.py
+++ trickGenerator.py
@@ -24,32 +24,32 @@ tricks = {
 	],
 
 	"output":[
-		"#desc1.capitalize# #desc2# #desc3# #trick#",
+		"#desc1.a# #desc2# #desc3# #trick#",
+		"#you.capitalize# #desc1.capitalize# #desc2# #desc3# #trick#",
 		"#desc3.capitalize# #trick# in to #desc1.a# #desc2# #trick#",
+		"#you.capitalize# #desc3.a# #trick# in to #desc1.a# #desc2# #trick#",
 		"#desc1.capitalize# #trick# #trick#",
-		"#desc1.capitalize# #desc2# #desc3# #trick# #suffix#",
-		"#desc1.capitalize# #desc2# #desc3# #trick#",
-		"#desc3.capitalize# #trick# in to #desc1.a# #desc2# #trick#",
-		"#desc1.capitalize# #trick# #trick#",
-		"#desc1.capitalize# #desc2# #desc3# #trick# #suffix#",
-		"#starter.capitalize# off the #ramp# and do #desc1.a# #trick#, "+str(starter())+" on to #distance.a# #obstacle# to do #desc3.a# #trick#",
+		"#you.capitalize# #desc1.a# #trick# #trick#",
+		"#desc1.a# #desc2# #desc3# #trick# #suffix#",
+		"#you.capitalize# #desc1.a# #desc2# #desc3# #trick# #suffix#",
+		"#starter.capitalize# off the #ramp# and do #desc1.a# #trick#, "+str(starter())+" on to #distance.a# #obstacle# while #you# #desc3.a# #trick#",
 		"#desc2# #trick# from the #distance# #obstacle#",
-		"#desc3# #desc2# #trick",
-		"#desc2# #trick# from the #distance# #obstacle#",
-		"#desc3# #desc2# #trick"
+		"#you.capitalize# #desc2.a# #trick# from the #distance# #obstacle#",
+		"#desc3# #desc2# #trick#",
+		"#you.capitalize# #desc3.a# #desc2# #trick#"
 	],
 
 
 	"desc1":[
-		"gnarly","radical","tubular","fabulous","sweet","sick","dope","eXtreme","fakie","reverse","cRaZy","ridiculous","badass","nollie", "boneless"
+		"gnarly","radical","tubular","fabulous","sweet","sick","dope","eXtreme","fakie","reverse","cRaZy","ridiculous","badass","nollie", "boneless", "no-comply"
 	],
 
 	"trick":[
-		"kickflip","McTwist","revert","shuvit","tre flip","wyrmride","flamingo","funk flip","christ air","ollie","boneless","hardflip","bonerflip","timewarp","heelflip","varial flip","laser flip","underflip", "casper", "truckstand"
+		"kickflip","McTwist","revert","shuvit","tre flip","wyrmride","flamingo","funk flip","christ air","ollie","boneless","hardflip","bonerflip","timewarp","heelflip","varial flip","laser flip","underflip", "casper", "truckstand", "ascension", "impossible"
 	],
 
 	"desc2":[
-	"1080°","900°","720°","360°","180°","railgrind","69°","420°"
+	"1080°","900°","720°","360°","180°","railgrind","69°","420°", "45°"
 	],
 
 	"desc3":[
@@ -57,7 +57,7 @@ tricks = {
 	],
 
 	"suffix":[
-		"with a li'l sauce","deluxe supreme","with fries","trainwreck","with a SWEEET landing", "with a nasty bail"
+		"with a li'l sauce","deluxe supreme","with fries","trainwreck","with a SWEEET landing", "with a nasty bail", "and the crowd goes wild!", "and everyone cheers", "over a flight of stairs", "over the grand canyon", "over the megaramp"
 	],
 
 	"ramp":[
@@ -72,6 +72,10 @@ tricks = {
 
 	"distance": [
 		"nearby", "far away", "large", "small", "indoor"
+	],
+
+	"you": [
+		"you pull off", "you try", "you stick", "you go for"
 	]
 }
 
